c3b66bb569ad8c8e979e77da44e71d908d5c5433
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-02-21 17:25:17 +0530

    Created window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 800;
+const HEIGHT: usize = 600;
+
+fn create_u32_colour(red: u8, green: u8, blue: u8) -> u32 {
+    ((red as u32) << 16) | ((green as u32) << 8) | blue as u32
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new("Test window", WIDTH, HEIGHT, WindowOptions::default())
+        .unwrap_or_else(|e| panic!("{}", e));
+
+    window.set_target_fps(60);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = create_u32_colour(0, 0, 0);
+        }
+
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
 }
