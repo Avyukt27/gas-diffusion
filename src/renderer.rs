2a29f7a6bf41f849b198712d6244149c040a8456
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-03-15 14:33:20 +0530

    Created new method for renderer

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,19 +1,54 @@
 use winit::window::Window;
 
-pub struct Renderer {
-    surface: wgpu::Surface<'static>,
+pub struct Renderer<'a> {
+    sim_width: u32,
+    sim_height: u32,
+
+    surface: wgpu::Surface<'a>,
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-
-    texture: wgpu::Texture,
-    texture_view: wgpu::TextureView,
-    sampler: wgpu::Sampler,
-
-    bind_group: wgpu::BindGroup,
-    pipeline: wgpu::RenderPipeline,
 }
 
-impl Renderer {
-    pub fn new(window: &Window, sim_width: u32, sim_height: u32) {}
+impl<'a> Renderer<'a> {
+    pub async fn new(window: &'a Window, sim_width: u32, sim_height: u32) -> Self {
+        let instance = wgpu::Instance::default();
+
+        let surface = instance.create_surface(window).unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor::default())
+            .await
+            .unwrap();
+
+        let size = window.inner_size();
+        let capabilities = surface.get_capabilities(&adapter);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: capabilities.formats[0],
+            width: size.width,
+            height: size.height,
+            present_mode: capabilities.present_modes[0],
+            alpha_mode: capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            sim_width,
+            sim_height,
+            surface,
+            device,
+            queue,
+            config,
+        }
+    }
 }
