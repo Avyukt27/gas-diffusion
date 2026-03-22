96f949376b63d7e9a6af35741b4967119e36bdf3
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-03-22 17:47:19 +0530

    Completely changed fiel structure

---
+++ native/src/main.rs
@@ -0,0 +1,8 @@
+use winit::event_loop::EventLoop;
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+    let mut app = core::app::App::new();
+    event_loop.run_app(&mut app).unwrap();
+}
